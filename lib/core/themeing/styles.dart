ea1da304c1cb152737fa9773bf5cfd09b2343527
nourhanbakry <nourhankhalebakry@gmail.com>
2024-08-24 01:01:15 +0300

    some fixes - added spaceing file for sized box - added font weight helper - added genral button & text form field - finshed login screen ui

--- lib/core/themeing/styles.dart
+++ lib/core/themeing/styles.dart
@@ -1,29 +1,66 @@
 import 'package:advanced_project/core/themeing/colors.dart';
+import 'package:advanced_project/core/themeing/font_weigth_helper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TextStyles {
-  static TextStyle font24Black700Weigth = TextStyle(
+  static TextStyle font24BlackBold = TextStyle(
     color: Colors.black,
     fontSize: 24.sp,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeigthHelper.bold,
   );
 
   static TextStyle font32BlueBold = TextStyle(
     color: ColorsManager.mainBlue,
     fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeigthHelper.bold,
   );
 
-  static TextStyle font13Grey = TextStyle(
+  static TextStyle font24BlueBold = TextStyle(
+    color: ColorsManager.mainBlue,
+    fontSize: 24.sp,
+    fontWeight: FontWeigthHelper.bold,
+  );
+
+  static TextStyle font13GreyRegular = TextStyle(
     color: ColorsManager.grey,
     fontSize: 13.sp,
-    fontWeight: FontWeight.normal,
+    fontWeight: FontWeigthHelper.regular,
   );
 
-  static TextStyle font16White700Weigth = const TextStyle(
+  static TextStyle font14GreyRegular = TextStyle(
+    color: ColorsManager.grey,
+    fontSize: 14.sp,
+    fontWeight: FontWeigthHelper.regular,
+  );
+
+  static TextStyle font14LightGreyRegular = TextStyle(
+    color: ColorsManager.lightGrey,
+    fontSize: 14.sp,
+    fontWeight: FontWeigthHelper.regular,
+  );
+
+  static TextStyle font16WhiteBold = const TextStyle(
     color: Colors.white,
     fontSize: 16,
-    fontWeight: FontWeight.w700,
+    fontWeight: FontWeigthHelper.bold,
+  );
+
+  static TextStyle font14DarkBlueMedium = TextStyle(
+    color: ColorsManager.darkBlue,
+    fontSize: 14.sp,
+    fontWeight: FontWeigthHelper.medium,
+  );
+
+  static TextStyle font14BlueRegular = const TextStyle(
+    color: ColorsManager.mainBlue,
+    fontSize: 14,
+    fontWeight: FontWeigthHelper.regular,
+  );
+
+  static TextStyle font14BlueSemiBold = const TextStyle(
+    color: ColorsManager.mainBlue,
+    fontSize: 14,
+    fontWeight: FontWeigthHelper.semiBold,
   );
 }
--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,13 +1,77 @@
+import 'package:advanced_project/core/themeing/spacing.dart';
+import 'package:advanced_project/core/themeing/styles.dart';
+import 'package:advanced_project/core/widgets/app_text_button.dart';
+import 'package:advanced_project/core/widgets/app_text_form_field.dart';
+import 'package:advanced_project/features/login/ui/widgets/already_have_account_text.dart';
+import 'package:advanced_project/features/login/ui/widgets/terms_and_condtions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final globalKey = GlobalKey<FormState>();
+  bool obScureText = true;
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 30.h),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Welcome Back", style: TextStyles.font24BlueBold),
+                verticalSpacing(8),
+                Text(
+                    "We're excited to have you back, can't wait to see what you've been up to since you last \nlogged in.",
+                    style: TextStyles.font14GreyRegular),
+                verticalSpacing(36),
+                Form(
+                  key: globalKey,
+                  child: Column(
+                    children: [
+                      const AppTextFormField(hintText: "Email"),
+                      verticalSpacing(18),
+                      AppTextFormField(
+                          suffixIcon: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  obScureText = !obScureText;
+                                });
+                              },
+                              child: Icon(obScureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility)),
+                          hintText: "Password",
+                          isObscureText: obScureText),
+                      verticalSpacing(24),
+                      Align(
+                          alignment: AlignmentDirectional.centerEnd,
+                          child: Text("Forgot Password?",
+                              style: TextStyles.font14BlueRegular)),
+                      verticalSpacing(40),
+                      AppTextButton(
+                          buttonText: "Login",
+                          textStyle: TextStyles.font16WhiteBold,
+                          onPressed: () {}),
+                      verticalSpacing(16),
+                      const TermsAndCondtionsText(),
+                      verticalSpacing(60),
+                      const AlreadyHaveAccountText(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
