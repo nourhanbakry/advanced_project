95432e77865359b89704d77785e8d1362f2c3f71
Nourhan Bakry <152170075+nourhanbakry@users.noreply.github.com>
2024-08-30 17:30:59 +0300

    Adroid Development & Production flavors - added launch.json for flavors config - changed android app name for each flavor - added 2 min files for each flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,8 +2,11 @@ import 'package:advanced_project/core/di/dependancy_injection.dart';
 import 'package:advanced_project/core/routing/app_router.dart';
 import 'package:advanced_project/doc_doc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async{
 setupGetIt();
+ // to fix text being hidden bug in flutter_screenUtil in release run
+  await ScreenUtil.ensureScreenSize();
   runApp(DocDocApp(appRouter: AppRouter() ));
 }
